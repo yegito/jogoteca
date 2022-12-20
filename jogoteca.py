a3fd4237185a523778e01a374f468a73022322b4
yegito <yuriegito@hotmail.com>
2022-12-20 12:22:08 -0300

    Initial commit

---
+++ jogoteca.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/')
+def Index():
+
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', novo="Novo Jogo")
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console )
+    lista.append(jogo)
+    return redirect("/")
+
+
+app.run(debug=True)
